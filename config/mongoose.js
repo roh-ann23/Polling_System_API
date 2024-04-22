05e01c85ad6c410f24bddaabaafc2966f9de911f
roh-ann23 <158121284+roh-ann23@users.noreply.github.com>
2024-04-22 23:14:30 +0530

    additional changes

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Polling_System');
+// mongoose.connect('mongodb://localhost:27017/Polling_System');
+mongoose.set('strictQuery', false);
+const dotenv = require('dotenv');
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL);
 
 // Code for using the cloud mongodb atlas 
-// const connectParams={
-//     useNewUrlParser:true,
-//     useUnifiedTopology:true
-// }
+const connectParams={
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}
 
 // Code for manually using the mongodb of local system
 
